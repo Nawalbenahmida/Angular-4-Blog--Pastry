468bee549a15a8fd32639c81cbcf36092c54ceb8
nawal75019 <nawalbenahmida19@hotmail.com>
2017-11-07 15:40:05 +0100

    component + routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        PostsComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: 'About',
+                component: AboutComponent,
+            },
+            {
+            path: 'Posts',
+            component: PostsComponent,
+            },
+
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
