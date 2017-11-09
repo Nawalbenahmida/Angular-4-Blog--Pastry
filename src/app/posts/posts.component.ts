72050b6fc90f2f2c4c58ff04128cbfdda2901419
nawal75019 <nawalbenahmida19@hotmail.com>
2017-11-09 14:07:45 +0100

    filtre date + style article

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,13 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { FilterPipe }from '../filter.pipe';
 
+import {SortPipe } from '../sort.pipe';
+
 import { Article } from '../article/article';
 
 const ARTICLES: Article[] =  [
    
-    {titre : "2 - Brownie tart croissant", desc: "candy wafer. Tart tart pudding sweet", image: "", date: new Date(1942, 1, 14)},
-     {titre: "1 - Fruitcake gummi ", desc: "Sweet jujubes gummies. Biscuit tootsie roll" , image:"" , date: new Date(1962, 3, 6)},
-    {titre: "3 - Sweet jujubes gummies. Biscuit", desc: "topping caramels fruitcake cupcake pudding oat cake. Donut muffin gummi", image: "", date: new Date(1995, 2, 6)}
+    {titre: "Cookie I love candy", desc: "Cookie caramels I love fruitcake cake. ", image: "../../assets/brooke-lark-203839.jpg", date: new Date(1995, 2, 6)},
+    
+     {titre: " Biscuit I love wafer ", desc: "Carrot cake pastry muffin. Tootsie roll cake " , image:"../../assets/courtney-prather-431861.jpg" , date: new Date(1962, 3, 6)},
+    
+    {titre : " I love cheesecake", desc: "I love ice cream sesame snaps jelly beans  ", image: "../../assets/isaac-chua-232353.jpg", date: new Date(1942, 1, 14)},
+    
+    {titre : "Cookie cupcake sweet roll", desc: "Lollipop gingerbread bonbon brownie tiramisu pie ", image: "../../assets/brian-chan-12168.jpg", date: new Date(2017, 4, 12)},
+    
+    {titre : "Biscuit I love jelly tiramisu", desc: "Bonbon cotton candy sesame snaps cotton candy", image: "../../assets/patrick-fore-389419.jpg", date: new Date(2000, 8, 20)},
+    
+    {titre : "Croissant chocolate fruitcake", desc: "Brownie gummi bears cookie halvah tart Halvah jelly beans ", image: "../../assets/stephanie-mccabe-65986.jpg", date: new Date(1988, 9, 10)},
+    
 ];
 
 @Component({
--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -1,28 +1,59 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { PostsComponent } from './posts/posts.component';
 import { Article } from './article/article';
 
 @Pipe({
-  name: 'sort'
+    name: 'sort'
 })
 
 export class SortPipe implements PipeTransform {
 
-  transform(value: any, b: any): any {
-      /*console.log(value);*/
-    return value.sort((a: any, b: any) => {
-           let date1 = new Date(a.date);
-        console.log(a)
-           let date2 = new Date(b.date);
+    transform(value: any, b: any): any {
+        let date = b;
 
-           if (date1 > date2) {
-               return 1;
-           } else if (date1 < date2) {
-               return +1;
-           } else {
-               return 0;
-           }
-         });
-  }
+        if(date){
+            console.log('date')
+            return value.sort((a: any, b: any) => {
+                let date1 = new Date(a.date);
+                let date2 = new Date(b.date);
+
+
+
+
+                if (date1 > date2) {
+                    return -1;
+                } else if (date1 < date2) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+
+            });
+            }
+        
+            else {
+                console.log('pas date')
+                return value.sort((a: any, b: any) => {
+                    const date1 = new Date(a.date);
+                    const date2 = new Date(b.date);
+
+                    if (date1 < date2) {
+                        return -1;
+                    } else if (date1 > date2) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+            };
+
+
+
+
+
+        
+    };
+
 
 }
